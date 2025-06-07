905b5e8f1c9baf05c39e5aa3e121e712c0b1f02f
Wenyi Wang <wenyiwang.us@gmail.com>
2025-06-07 18:07:51 +0000

    XQueue implementation completed. Worker class has issues when initializing, checkpoint current progress for debugging.

--- taskflow/core/worker.hpp
+++ taskflow/core/worker.hpp
@@ -57,6 +57,11 @@ class Worker {
 
   public:
 
+#if TF_USE_XQUEUE
+    Worker(const size_t nworkers) : _xq(nworkers) {}
+    ~Worker() = default;
+#endif // TF_USE_XQUEUE
+
     /**
     @brief queries the worker id associated with its parent executor
 
@@ -70,12 +75,19 @@ class Worker {
     @brief queries the size of the queue (i.e., number of enqueued tasks to
            run) associated with the worker
     */
+
+    #if !TF_USE_XQUEUE
     inline size_t queue_size() const { return _wsq.size(); }
+    #endif // TF_USE_XQUEUE
     
     /**
     @brief queries the current capacity of the queue
     */
+    #if TF_USE_XQUEUE
+    inline size_t queue_capacity() const { return static_cast<size_t>(_xq.capacity()); }
+    #else
     inline size_t queue_capacity() const { return static_cast<size_t>(_wsq.capacity()); }
+    #endif // TF_USE_XQUEUE
     
     /**
     @brief acquires the associated executor
@@ -86,6 +98,10 @@ class Worker {
     @brief acquires the associated thread
     */
     std::thread& thread() { return _thread; }
+    
+#if TF_USE_XQUEUE
+    BoundedXQueue<Node*> _xq;
+#endif // TF_USE_XQUEUE
 
   private:
   
@@ -103,8 +119,11 @@ class Worker {
     
     std::default_random_engine _rdgen;
     //std::uniform_int_distribution<size_t> _udist;
-
+#if !TF_USE_XQUEUE
     BoundedTaskQueue<Node*> _wsq;
+#endif // !TF_USE_XQUEUE
+
+
 
     //TF_FORCE_INLINE size_t _rdvtm() {
     //  auto r = _udist(_rdgen);
@@ -113,6 +132,118 @@ class Worker {
 
 };
 
+#if TF_USE_XQUEUE
+// ----------------------------------------------------------------------------
+// XQueue Functions Definitions
+// ----------------------------------------------------------------------------
+
+// TODO:
+// []: Try if inline works as other functions in tf are inlined
+template <typename T, size_t LogSize>
+BoundedXQueue<T, LogSize>::BoundedXQueue(const size_t nworkers) : _nworkers(nworkers) {
+  _dequeues = new XDequeue[_nworkers];
+  for (int64_t i = 0; i < _nworkers; i++) {
+    _dequeues[i].head = 0;
+    _dequeues[i].tail = 0;
+  }
+}
+
+// template <typename T, size_t LogSize>
+// BoundedXQueue<T, LogSize>::BoundedXQueue(std::vector<tf::Worker> &workers)
+//     : _nworkers(workers.size()), _workers(workers) {
+//   // WW: We need to enforce that the executor will pin workers to
+//   // threads/cores WW: Or is this not necessary?
+//   // TODO: Optimizations not considered yet:
+//   // 1. Cache optimization
+//   // 2. Allocation optimization (Should we use a freelist?)
+//   _dequeues = new XDequeue[_nworkers];
+
+//   // For each thread, allocate a row of size BufferSize
+//   for (int64_t i = 0; i < _nworkers; i++) {
+//     _dequeues[i].head = 0;
+//     _dequeues[i].tail = 0;
+//   }
+// }
+
+template <typename T, size_t LogSize>
+BoundedXQueue<T, LogSize>::~BoundedXQueue() {
+  delete [] _dequeues;
+}
+
+template <typename T, size_t LogSize>
+TaskQueueCode BoundedXQueue<T, LogSize>::push(T item) {
+  int num_tries = 0;
+  size_t target_worker_id = _worker_id + _last_q;
+  target_worker_id = (target_worker_id > _nworkers - 1)
+                          ? (target_worker_id - _nworkers)
+                          : target_worker_id;
+  
+  Worker& target_worker = (*_workers)[target_worker_id];
+  while (target_worker._xq._dequeues[_last_q]
+              .dequeue[target_worker._xq._dequeues[_last_q].head] !=
+          nullptr) {
+    num_tries++;
+    if (num_tries < 25) {
+      continue;
+    }
+    return TaskQueueCode::TASK_NOT_PUSHED;
+  }
+  auto target_dequeue = target_worker._xq._dequeues[_last_q];
+  target_dequeue.dequeue[target_dequeue.head] = item;
+  target_dequeue.head = (target_dequeue.head + 1) & DequeueMask;
+  target_worker._xq._last_q_accessed = _last_q;
+  return tf::TaskQueueCode::TASK_PUSHED;
+}
+
+template <typename T, size_t LogSize>
+T BoundedXQueue<T, LogSize>::pop(size_t &last_qid) {
+  T item {nullptr};
+  // First, pop tasks from my own master queue
+  if (_dequeues[0].dequeue[_dequeues[0].tail] != nullptr) {
+    item = _dequeues[0].dequeue[_dequeues[0].tail];
+    _dequeues[0].dequeue[_dequeues[0].tail] = nullptr;
+    _dequeues[0].tail = (_dequeues[0].tail + 1) & DequeueMask;
+    return item;
+  }
+
+  // Then, pop tasks from the last accessed queue
+  if (_last_q_accessed > 0) {
+    auto target_dequeue = _dequeues[_last_q_accessed];
+    if (target_dequeue.dequeue[target_dequeue.tail] != nullptr) {
+      item = target_dequeue.dequeue[target_dequeue.tail];
+      target_dequeue.dequeue[target_dequeue.tail] = nullptr;
+      target_dequeue.tail = (target_dequeue.tail + 1) & DequeueMask;
+      last_qid = _last_q_accessed;
+      return item;
+    }
+  }
+
+  // Then try pop from the last queue
+  for (size_t qid = _nworkers - 1; qid > 0; qid--) {
+    auto target_dequeue = _dequeues[qid];
+    if (target_dequeue.dequeue[target_dequeue.tail] != nullptr) {
+      item = target_dequeue.dequeue[target_dequeue.tail];
+      target_dequeue.dequeue[target_dequeue.tail] = nullptr;
+      target_dequeue.tail = (target_dequeue.tail + 1) & DequeueMask;
+      last_qid = qid;
+      return item;
+    }
+  }
+  // Then try to pop from the rest of the queues
+  for (size_t qid = _nworkers - 1; qid > last_qid; qid--) {
+    auto target_dequeue = _dequeues[qid];
+    if (target_dequeue.dequeue[target_dequeue.tail] != nullptr) {
+      item = target_dequeue.dequeue[target_dequeue.tail];
+      target_dequeue.dequeue[target_dequeue.tail] = nullptr;
+      target_dequeue.tail = (target_dequeue.tail + 1) & DequeueMask;
+      last_qid = qid;
+      return item;
+    }
+  }
+
+  return item; // which is a nullptr
+}
+#endif // TF_USE_XQUEUE
 
 // ----------------------------------------------------------------------------
 // Per-thread
@@ -185,6 +316,7 @@ inline size_t WorkerView::id() const {
   return _worker._id;
 }
 
+#if !TF_USE_XQUEUE
 // Function: queue_size
 inline size_t WorkerView::queue_size() const {
   return _worker._wsq.size();
@@ -194,6 +326,7 @@ inline size_t WorkerView::queue_size() const {
 inline size_t WorkerView::queue_capacity() const {
   return static_cast<size_t>(_worker._wsq.capacity());
 }
+#endif // !TF_USE_XQUEUE
 
 // ----------------------------------------------------------------------------
 // Class Definition: WorkerInterface
