793b12be1f383634b42826c28ca4c25e8c225194
Wenyi Wang <wenyiwang.us@gmail.com>
2025-06-23 13:23:19 +0000

    Add skynet, async_task and integrate benchmarks as they use async tasks

--- selected_benchmarks/skynet/main.cpp
+++ selected_benchmarks/skynet/main.cpp
@@ -1,38 +1,38 @@
-#include "async_task.hpp"
+// The skynet benchmark as described here:
+// https://github.com/atemerev/skynet
+
+#include "skynet.hpp"
 #include <CLI11.hpp>
 
-void bench_async_task(
+void bench_skynet(
   const std::string& model,
   const unsigned num_threads,
-  const unsigned num_rounds
+  const unsigned num_rounds,
+  const size_t MaxDepth
   ) {
 
   std::cout << std::setw(12) << "size"
             << std::setw(12) << "threads"
             << std::setw(12) << "runtime"
             << std::endl;
-  int S = 2097152;
-  // for(int S=1; S<=2097152; S<<=1) {
 
+  // for(size_t MaxDepth=1; MaxDepth<=8; MaxDepth++) { 
     double runtime {0.0};
 
     for(unsigned j=0; j<num_rounds; ++j) {
       if(model == "tf") {
-        runtime += measure_time_taskflow(num_threads, S).count();
-      }
-      else if(model == "std") {
-        runtime += measure_time_std(num_threads, S).count();
-      }
-      else if(model == "omp") {
-        runtime += measure_time_omp(num_threads, S).count();
+        runtime += measure_time_taskflow(num_threads, MaxDepth).count();
       }
       // else if(model == "tbb") {
-      //   runtime += measure_time_tbb(num_threads, S).count();
+      //   runtime += measure_time_tbb(num_threads, MaxDepth).count();
       // }
+      else if(model == "omp") {
+        runtime += measure_time_omp(num_threads, MaxDepth).count(); 
+      }
       else assert(false);
     }
 
-    std::cout << std::setw(12) << S
+    std::cout << std::setw(12) << MaxDepth
               << std::setw(12) << num_threads
               << std::setw(12) << runtime / num_rounds / 1e3
               << std::endl;
@@ -41,31 +41,35 @@ void bench_async_task(
 
 int main(int argc, char* argv[]) {
 
-  CLI::App app{"AsyncTask"};
+  CLI::App app{"Skynet"};
 
   unsigned num_threads {1};
   app.add_option("-t,--num_threads", num_threads, "number of threads (default=1)");
-
+  
   unsigned num_rounds {1};
   app.add_option("-r,--num_rounds", num_rounds, "number of rounds (default=1)");
 
+  size_t MaxDepth {4};
+  app.add_option("-d,--max_depth", MaxDepth, "max depth (default=1)");
+
   std::string model = "tf";
-  app.add_option("-m,--model", model, "model name std|omp|tf|tbb (default=tf)")
+  app.add_option("-m,--model", model, "model name tf|tbb|omp (default=tf)")
      ->check([] (const std::string& m) {
-        if(m != "std" && m != "omp" && m != "tf" && m != "tbb") {
-          return "model name should be \"std\", \"omp\", \"tbb\", or \"tf\"";
+        if(m != "tf" && m != "tbb" && m != "omp") {
+          return "model name should be \"tbb\", \"omp\", or \"tf\"";
         }
         return "";
      });
 
+
   CLI11_PARSE(app, argc, argv);
 
-  std::cout << "model=" << model << ' '
+  std::cout << "model="       << model << ' '
             << "num_threads=" << num_threads << ' '
-            << "num_rounds=" << num_rounds << ' '
+            << "num_rounds="  << num_rounds << ' '
             << std::endl;
 
-  bench_async_task(model, num_threads, num_rounds);
+  bench_skynet(model, num_threads, num_rounds, MaxDepth);
 
   return 0;
 }
